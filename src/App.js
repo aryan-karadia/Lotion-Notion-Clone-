63b2daf8a11e80c93974e8f926f770b69ee37b8f
Aryan Karadia <karadiaaryan@gmail.com>
2023-02-28 15:34:28 -0700

    header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from './components/Header';
+
 function App() {
-  return <h1>Lotion</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Lotion</h1>
+            <h2>Like Notion, but worse</h2>
+        </div>
+    );
+}
+
+export default Header;
