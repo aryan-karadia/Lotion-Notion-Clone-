e2aceb636d82ccf3aadfcbd5dad52da892fb4b3d
Aryan Karadia <karadiaaryan@gmail.com>
2023-02-28 17:45:16 -0700

    header jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,14 @@
 
 const Header = () => {
     return (
-        <div>
-            <h1>Lotion</h1>
-            <h2>Like Notion, but worse</h2>
-        </div>
+        <header>
+            <span className="menu-toggle">&#9776;</span>
+            <div className="header-text">                
+                <h1>Lotion</h1>
+                <p>Like Notion, but worse.</p>
+            </div>
+            <span className="filler"></span>
+        </header>
     );
 }
 
