206132228b8de643b0f1987958cbba0d86e4e0d6
Aryan Karadia <karadiaaryan@gmail.com>
2023-02-28 17:45:22 -0700

    sideMenu jsx

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,16 @@
+
+const SideMenu = () => {
+    return (
+        <div className="side-menu">
+            <div className="side-header">
+                <h1>Notes</h1>
+                <span className="new-note">&#43;</span>
+            </div>
+            <div id="note-titles">
+                <p>No Notes Yet</p>
+            </div>
+        </div>
+    )
+}
+
+export default SideMenu;
