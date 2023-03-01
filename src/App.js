1b30f12c62f8768aa20467af078d4e143635116b
Aryan Karadia <karadiaaryan@gmail.com>
2023-02-28 17:45:37 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Notes from './pages/Notes';
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Notes />} />
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
