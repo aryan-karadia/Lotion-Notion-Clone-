786554b8cd6b533b7f9b954030b08ec60c6ed5f9
Aryan Karadia <karadiaaryan@gmail.com>
2023-02-28 17:45:28 -0700

    notes homepage jsx

---
+++ src/pages/Notes.jsx
@@ -0,0 +1,11 @@
+import SideMenu from "../components/SideMenu";
+
+const Notes = () => {
+    return (
+        <div className="notes">
+            <SideMenu />
+        </div>
+    )
+}
+
+export default Notes;
