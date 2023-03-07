ac10f73aff38690218eeb031466a5bd3c192d60c
Aryan Karadia <aryan.karadia@ucalgary.ca>
2023-03-06 19:19:12 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Notes from './pages/Notes';
 import Layout from './components/Layout';
 import Edit from './pages/Edit';
+import NoteView from './pages/NoteView';
 
 function App() {
 
@@ -11,8 +12,10 @@ function App() {
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/Notes"/>} />
-              <Route path="/Notes" element={<Notes />} />
               <Route path="Notes/:id/edit" element={<Edit />} />
+              <Route path="/Notes" element={<Notes />} />
+              <Route path="Notes/:id/edit/:id" element={<Navigate to="/Notes/:id" />} />
+              <Route path="Notes/:id" element={<NoteView />} />
             </Route>
           </Routes>
       </BrowserRouter>
